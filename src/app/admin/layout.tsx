921842c6b6cad5e8c2dc2dad772fc477cdf11ecf
Mohammad Imran <imrancorna@gmail.com>
2026-01-18 14:07:49 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -47,15 +47,16 @@ export default function AdminLayout({
           <SidebarMenu>
             {menuItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
-                  <SidebarMenuButton
-                    isActive={pathname === item.href}
-                    tooltip={{ children: item.label }}
-                  >
+                <SidebarMenuButton
+                  asChild
+                  isActive={pathname === item.href}
+                  tooltip={{ children: item.label }}
+                >
+                  <Link href={item.href}>
                     <item.icon />
                     <span>{item.label}</span>
-                  </SidebarMenuButton>
-                </Link>
+                  </Link>
+                </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
